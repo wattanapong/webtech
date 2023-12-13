4afa0fb9ea7a53ba1ede2580e32c7a1358d13a9c
WATTANAPONG SUTTAPAK <wattanapong.su@up.ac.th>
2023-12-14 00:39:06 +0700

    add lab05 and lab06

--- lab05_nodejs/ex1/server.js
+++ lab05_nodejs/ex1/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+var url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,13 +7,16 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+
 //   res.setHeader('Content-Type', 'application/json');
 //   res.setHeader('Content-Type', 'application/pdf');
 //   res.setHeader('Content-Type', 'text/plain');
   res.write('<h1>Hello Web Programming Technology</h1>')
-  res.end('Hello, World!\n');
+  var params = url.parse(req.url, true).query;
+  res.end('Hello, World!\n' + params.name);
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 }); 
+
--- lab05_nodejs/ex2/Readme.md
+++
@@ -1,6 +0,0 @@
-# Introduction to Express
-- npm script 
-
-```
-npm run wstart
-```
