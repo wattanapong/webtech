a6101c0187c8af89c043b0d8898bcf7b526f2d28
wattanapong.su <wattanapong.su@outlook.com>
2026-01-27 22:46:01 +0700

    add chapter9 ex2 to fetch data

--- lab09_promise/ex2/index.js
+++ lab09_promise/ex2/index.js
@@ -7,8 +7,6 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use('/customers', require('./router/customers'));
-app.use('/order', require('./router/order'));
-app.use('/orderdetails', require('./router/orderdetails'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
